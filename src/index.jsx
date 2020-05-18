c120e69d65f4ba5eb6b413fb5044daa8256f5bb5
Dzmitry Tsebruk <mikhama@tut.by>
2020-05-18 22:49:15 +0300

    feat: [6-53] learn fetch api

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,8 @@
-global.console.log('hello');
+const getResource = async (url) => {
+  const res = await fetch(url);
+  const body = await res.json();
+  return body;
+};
+
+getResource('https://swapi.dev/api/people/1/')
+  .then((res) => console.log(res));
