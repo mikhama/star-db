d559170e1b5932b5ac49eb5c044d041250733247
Dzmitry Tsebruk <mikhama@tut.by>
2020-05-21 23:32:54 +0300

    feat: [7-66] learn how to use componentDidMount()

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
@@ -7,21 +7,36 @@ import PersonDetails from '../person-details';
 import PlanetDetails from '../planet-details';
 import StarshipDetails from '../starship-details';
 
-const App = () => (
-  <div className="app">
-    <Header />
-    <main className="container page-layout">
-      <RandomPlanet />
-      <div className="row">
-        <div className="col-md">
-          <ItemList />
-        </div>
-        <div className="col-md">
-          <PersonDetails />
-        </div>
-      </div>
-    </main>
-  </div>
-);
+class App extends Component {
+  state = {
+    selectedPerson: null,
+  }
 
+  onPersonSelected = (id) => {
+    this.setState({
+      selectedPerson: id,
+    });
+  };
+
+  render = () => {
+    const { selectedPerson } = this.state;
+
+    return (
+      <div className="app">
+        <Header />
+        <main className="container page-layout">
+          <RandomPlanet />
+          <div className="row">
+            <div className="col-md">
+              <ItemList onItemSelected={this.onPersonSelected} />
+            </div>
+            <div className="col-md">
+              <PersonDetails personId={selectedPerson} />
+            </div>
+          </div>
+        </main>
+      </div>
+    );
+  }
+}
 export default App;
--- src/components/item-list/item-list.jsx
+++ src/components/item-list/item-list.jsx
@@ -1,26 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { PropTypes } from 'prop-types';
 
-const ItemList = () => (
-  <ul className="list-group item-list">
-    <li className="list-group-item">
-      Cras justo odio
-    </li>
-    <li className="list-group-item">
-      Dapibus ac facilisis in
-    </li>
-    <li className="list-group-item">
-      Morbi leo risus
-    </li>
-    <li className="list-group-item">
-      Cras justo odio
-    </li>
-    <li className="list-group-item">
-      Dapibus ac facilisis in
-    </li>
-    <li className="list-group-item">
-      Morbi leo risus
-    </li>
-  </ul>
-);
+import SwapiService from '../../services/swapi-service';
+import Spinner from '../spinner';
+
+class ItemList extends Component {
+  swapiService = new SwapiService();
+
+  state = {
+    peopleList: null,
+  }
+
+  async componentDidMount() {
+    const peopleList = await this.swapiService.getAllPeople();
+
+    this.setState({ peopleList });
+  }
+
+  renderItems = (items) => {
+    const { onItemSelected } = this.props;
+
+    return items.map(({ id, name }) => (
+      // eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions
+      <li
+        className="list-group-item"
+        key={id}
+        onClick={() => onItemSelected(id)}
+      >
+        {name}
+      </li>
+    ));
+  }
+
+  render = () => {
+    const { peopleList } = this.state;
+
+    if (!peopleList) {
+      return <Spinner />;
+    }
+
+    const items = this.renderItems(peopleList);
+
+    return (
+      <ul className="list-group item-list">
+        {items}
+      </ul>
+    );
+  }
+}
+
+ItemList.propTypes = {
+  onItemSelected: PropTypes.func.isRequired,
+};
 
 export default ItemList;
