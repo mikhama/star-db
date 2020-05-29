3ef4f1109d72f1e706615397bca0c14b47b69727
Dzmitry Tsebruk <mikhama@tut.by>
2020-05-29 18:14:49 +0300

    feat: [10-107] learn about Link component

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,36 +1,37 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const Header = ({ onServiceChange }) => (
   <nav className="navbar navbar-expand navbar-dark bg-primary header">
     <div className="container">
-      <a className="navbar-brand" href="./">
+      <Link className="navbar-brand" to="/">
         <i className="fa fa-star-o" aria-hidden="true" />
         {' '}
         Database
-      </a>
+      </Link>
 
       <ul className="navbar-nav">
         <li className="nav-item active">
-          <a className="nav-link" href="./">
+          <Link className="nav-link" to="/people">
             <i className="fa fa-users" aria-hidden="true" />
             {' '}
             People
-          </a>
+          </Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="./">
+          <Link className="nav-link" to="/planets">
             <i className="fa fa-globe" aria-hidden="true" />
             {' '}
             Planets
-          </a>
+          </Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="./">
+          <Link className="nav-link" to="/starships">
             <i className="fa fa-ship" aria-hidden="true" />
             {' '}
             Starships
-          </a>
+          </Link>
         </li>
       </ul>
 
